b6678f6cb3bbc0ccfb09d758b927019dc5dd6d0c
Think042 <21521475@gm.uit.edu.vn>
2024-08-09 14:30:47 +0700

    seperate post and user from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { UserService } from './user/user.service';
 import { PostService } from './post/post.service';
 import { PrismaService } from './prisma.service';
+import { UserController } from './user/user.controller';
+import { PostController } from './post/post.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UserController, PostController],
   providers: [AppService, UserService, PostService, PrismaService],
 })
 export class AppModule {}
