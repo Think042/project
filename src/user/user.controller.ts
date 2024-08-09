81966db3a47a37510096c84eaac6ccd6cec33e8f
Think042 <21521475@gm.uit.edu.vn>
2024-08-09 15:31:04 +0700

    update user model, add delete api, remove controller test files

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Body, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User as UserModel } from '@prisma/client';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('user')
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+
+  @Get('users')
+  async getAllUsers(): Promise<UserModel[]> {
+    return this.userService.users({
+      where: { isDeleted: false },
+    });
+  }
+
+  @Put('user/:id')
+  async DeleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: { isDeleted: true },
+    });
+  }
+}
